163b659e8ca3b3afb11da0330a20c7c8f24ff30e
Bastien <bastien.delforge@etu.imt-lille-douai.fr>
2020-06-12 10:25:19 +0200

    Dropping NaN values from dataframe. Plotting scatter_matrix.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('red_wines.csv')
+data = pd.read_csv('red_wines.csv')
 
 # 2.2. Preparing data
-df.plot()
+df = data.dropna()
+print("Old data frame length:", len(data))
+print("New data frame length:", len(df))
+print("Number of rows with at least 1 NA value: ", (len(data) - len(df)))
+print("We removed ", ((len(data) - len(df)) / len(data)), "% of total amount of values.")
 
+test1 = data.isnull()
+test2 = df.isnull()
+
+test = pd.plotting.scatter_matrix(df, alpha=0.2, diagonal='hist')
 plt.show()
 
 print("Hello IDS project")
