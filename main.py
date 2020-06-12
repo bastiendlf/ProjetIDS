8c575c4ed7b93712f367052f8ae51b3f5eb39b38
Bastien <bastien.delforge@etu.imt-lille-douai.fr>
2020-06-12 09:45:41 +0200

    Adding short README file. Plotting dataframe raw values.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
-table = pd.read_csv('red_wines.csv')
+df = pd.read_csv('red_wines.csv')
 
-print("Hello IMT Lille Douai")
+# 2.2. Preparing data
+df.plot()
+
+plt.show()
+
+print("Hello IDS project")
