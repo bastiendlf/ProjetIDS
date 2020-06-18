610658b3f90658a8c040ffba78c56c542e1801fd
Bastien <bastien.delforge@etu.imt-lille-douai.fr>
2020-06-18 11:53:42 +0200

    fixing indent issues

--- perceptron.py
+++ perceptron.py
@@ -10,36 +10,34 @@ class Perceptron:
         self.threshold = threshold  # max number of iteration
         self.learning_rate = learning_rate  # it represents Êta in the lesson
 
+    def predict(self, x_values: pd.DataFrame):
+        """
+        TODO Write documentation
+        :param self:
+        :param x_values:
+        :return:
+        """
 
-def predict(self, x_values: pd.DataFrame):
-    """
-    TODO Write documentation
-    :param self:
-    :param x_values:
-    :return:
-    """
+        y_prediction = pd.Series()
 
-    y_prediction = pd.Series()
+        for current_individual in x_values:
+            sign_function = self.theta[0] + np.dot(current_individual, self.theta[1:])  # computing Theta * x + Theta0
 
-    for current_individual in x_values:
-        sign_function = self.theta[0] + np.dot(current_individual, self.theta[1:])  # computing Theta * x + Theta0
+            if sign_function > 0:
+                y_prediction.append(1)
+            else:
+                y_prediction.append(-1)
 
-        if sign_function > 0:
-            y_prediction.append(1)
-        else:
-            y_prediction.append(-1)
+        return y_prediction
 
-    return y_prediction
-
-
-def fit(self, x_train: pd.core.frame.DataFrame, y_train: pd.core.series.Series):
-    """
-    TODO Write documentation
-    :param self:
-    :param x_train:
-    :param y_train:
-    :return:
-    """
-    for _ in range(self.threshold):
-        for x, y in zip(x_train, y_train):
-            pass
+    def fit(self, x_train: pd.core.frame.DataFrame, y_train: pd.core.series.Series):
+        """
+        TODO Write documentation
+        :param self:
+        :param x_train:
+        :param y_train:
+        :return:
+        """
+        for _ in range(self.threshold):
+            for x, y in zip(x_train, y_train):
+                pass
