a4037a575a7937f475e2323dbe253ab50d632de0
Bastien <bastien.delforge@etu.imt-lille-douai.fr>
2020-06-18 09:07:46 +0200

    changing data structures in main

--- main.py
+++ main.py
@@ -14,19 +14,20 @@ feature_cols = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual su
                 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
 
 # 2.2. Preparing data
-df_without_outliers = remove_outliers(data)
-df_replace_outliers = change_outliers_by_median(data)
+df_dict = dict()
+df_dict["remove_outliers"] = remove_outliers(data)
+df_dict["replace_outliers"] = change_outliers_by_median(data)
 
 # plot_dataframe_columns(data)
-# plot_dataframe_columns(df_without_outliers)
-# plot_dataframe_columns(df_replace_outliers)
+# plot_dataframe_columns(df_dict["remove_outliers"])
+# plot_dataframe_columns(df_dict["replace_outliers"])
 
-# plot_scatter_matrix(df_without_outliers)
-# plot_scatter_matrix(df_replace_outliers)
+# plot_scatter_matrix(df_dict["remove_outliers"])
+# plot_scatter_matrix(df_dict["replace_outliers"])
 
-# correlation1 = correlation_table(df_without_outliers)
-correlation2 = correlation_table(df_replace_outliers)
-print(correlation2)
+# correlation1 = correlation_table(df_dict["remove_outliers"])
+# correlation2 = correlation_table(df_dict["replace_outliers"])
+
 
 # distribution of data in each class: good or bad
 good_wines = data.loc[data['quality'] == 1].shape[0]
@@ -35,18 +36,19 @@ print("\nProportion in classes: \n", "Good wines:", good_wines / len(data) * 100
       "Bad wines:", bad_wines / len(data) * 100, "%\n")
 
 # center and reduce
-center_df = pd.DataFrame(preprocessing.scale(df_without_outliers, with_mean='True', with_std='True'), columns=columns)
-center_df2 = pd.DataFrame(preprocessing.scale(df_replace_outliers, with_mean='True', with_std='True'), columns=columns)
+df_dict["replace_center"] = pd.DataFrame(
+    preprocessing.scale(df_dict["replace_outliers"], with_mean='True', with_std='True'), columns=columns)
+df_dict["remove_center"] = pd.DataFrame(
+    preprocessing.scale(df_dict["remove_outliers"], with_mean='True', with_std='True'), columns=columns)
 
-print("***************Center values***************")
-# eval_all_classifiers(x_values=center_df[feature_cols], y_values=df_without_outliers.quality)
-eval_all_classifiers(
-    x_values=center_df2[feature_cols],
-    y_values=df_replace_outliers.quality)
+# 2.4. Training classifiers
+print("***************Training classifiers with centered and reduced values***************")
+eval_all_classifiers(x_values=df_dict["remove_center"][feature_cols], y_values=df_dict["remove_outliers"].quality)
+# eval_all_classifiers(x_values=df_dict["replace_center"][feature_cols], y_values=df_dict["replace_outliers"].quality)
 
 # print("***************Raw values***************")
-# eval_all_classifiers(x_values=df_without_outliers[feature_cols], y_values=df_without_outliers.quality)
+# eval_all_classifiers(x_values=df_dict["remove_outliers"][feature_cols], y_values=df_dict["remove_outliers"].quality)
 
 print("Goodbye IDS project")
 
-manual_cross_validation(x_values=center_df[feature_cols], y_values=df_without_outliers.quality)
+manual_cross_validation(x_values=df_dict["remove_center"][feature_cols], y_values=df_dict["remove_outliers"].quality)
