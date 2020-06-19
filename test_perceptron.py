491d46dfc7786f633860c7b779c36c87d7d668f4
Bastien <bastien.delforge@etu.imt-lille-douai.fr>
2020-06-19 09:51:06 +0200

    adding comments

--- test_perceptron.py
+++ test_perceptron.py
@@ -15,7 +15,7 @@ perceptron = Perceptron(nb_input=2, learning_rate=0.01, threshold=10)
 
 dict1 = {'A': [1, 1, 0, 0], 'B': [1, 0, 1, 0]}
 list1 = [1, -1, -1, -1]
-plot_values(dict1["A"], dict1["B"], list1)
+
 
 x_train = pd.DataFrame(dict1)
 y_train = pd.Series(list1)
@@ -28,10 +28,11 @@ print('(1 1) ->', perceptron.predict(pd.DataFrame({'A': [1], 'B': [1]})), ': exc
 print('(1 0) ->', perceptron.predict(pd.DataFrame({'A': [1], 'B': [0]})), ': excepted -1.')
 print('(0 1) ->', perceptron.predict(pd.DataFrame({'A': [0], 'B': [1]})), ': excepted -1.')
 print('(0 0) ->', perceptron.predict(pd.DataFrame({'A': [0], 'B': [0]})), ': excepted -1.')
+plot_values(dict1["A"], dict1["B"], list1)
 
 dict2 = {'A': [1, 1, 0, 0], 'B': [1, 0, 1, 0]}
 list2 = [1, 1, 1, -1]
-plot_values(dict2["A"], dict2["B"], list2)
+
 
 x_train2 = pd.DataFrame(dict2)
 y_train2 = pd.Series(list2)
@@ -46,13 +47,13 @@ print('(1 1) ->', perceptron2.predict(pd.DataFrame({'A': [1], 'B': [1]})), ': ex
 print('(1 0) ->', perceptron2.predict(pd.DataFrame({'A': [1], 'B': [0]})), ': excepted 1.')
 print('(0 1) ->', perceptron2.predict(pd.DataFrame({'A': [0], 'B': [1]})), ': excepted 1.')
 print('(0 0) ->', perceptron2.predict(pd.DataFrame({'A': [0], 'B': [0]})), ': excepted -1.')
+plot_values(dict2["A"], dict2["B"], list2)
 
 x1 = [0.1, 0.2, 0.5, 0.75, 0.23, 0.45, 13, 10, 9, 12, 11, 10.5]
 x2 = [10, 11, 10.5, 13, 11.5, 12, 0.5, 1, 0.2, 0.3, 1.2, 0.33]
 
 dict3 = {'A': x1, 'B': x2}
 list3 = [1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1]
-plot_values(x1, x2, list3)
 
 x_train3 = pd.DataFrame(dict3)
 y_train3 = pd.Series(list3)
@@ -66,3 +67,4 @@ print('(1 11) ->', perceptron3.predict(pd.DataFrame({'X1': [1], 'X2': [11]})), '
 print('(2 12) ->', perceptron3.predict(pd.DataFrame({'X1': [2], 'X2': [12]})), ': excepted 1.')
 print('(10 0) ->', perceptron3.predict(pd.DataFrame({'X1': [10], 'X2': [0]})), ': excepted -1.')
 print('(12 2) ->', perceptron3.predict(pd.DataFrame({'X1': [12], 'X2': [2]})), ': excepted -1.')
+plot_values(x1, x2, list3)
