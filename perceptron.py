e81381c55a44c998804d3b87ee7429c62e54bc4b
Bastien <bastien.delforge@etu.imt-lille-douai.fr>
2020-06-19 09:36:28 +0200

    adding comments

--- perceptron.py
+++ perceptron.py
@@ -8,7 +8,7 @@ class Perceptron:
 
     def __init__(self, nb_input: int, learning_rate: float = 0.001, threshold: int = 100):
         self.theta = np.zeros(nb_input + 1)  # we add 1 line because of theta0
-        # Notice that we choose to init this matrix with zeros but we could have innit it with random values
+        # We choose to init this matrix with zeros but we could init it with random values
         self.threshold = threshold  # max number of iteration
         self.learning_rate = learning_rate  # it represents Êta in the lesson
 
@@ -19,6 +19,7 @@ class Perceptron:
         :param x_values:
         :return:
         """
+        # prediction = sign(Theta * x + Theta0)
         sign_function = np.dot(x_values, self.theta[1:]) + self.theta[0]
         return 1.0 if sign_function > 0. else -1.0
 
