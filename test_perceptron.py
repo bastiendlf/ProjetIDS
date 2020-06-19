b97cfd1abbe2d02f2c108869e91cfe7b141e6cad
Bastien <bastien.delforge@etu.imt-lille-douai.fr>
2020-06-19 09:54:07 +0200

    adding title to plots

--- test_perceptron.py
+++ test_perceptron.py
@@ -3,11 +3,11 @@ from perceptron import Perceptron
 import matplotlib.pyplot as plt
 
 
-def plot_values(x, y, eval):
+def plot_values(x, y, eval, title):
     for i in range(len(x)):
         color = "blue" if eval[i] == 1 else "red"
         plt.scatter(x[i], y[i], c=color, marker='o')
-    plt.title('Data visualization')
+    plt.title(title)
     plt.show()
 
 
@@ -22,13 +22,13 @@ y_train = pd.Series(list1)
 
 perceptron.fit(x_train, y_train)
 
-print("*****Testing perceptron for AND dor*****")
+print("*****Testing perceptron for AND door*****")
 print("Input : Output")
 print('(1 1) ->', perceptron.predict(pd.DataFrame({'A': [1], 'B': [1]})), ': excepted 1.')
 print('(1 0) ->', perceptron.predict(pd.DataFrame({'A': [1], 'B': [0]})), ': excepted -1.')
 print('(0 1) ->', perceptron.predict(pd.DataFrame({'A': [0], 'B': [1]})), ': excepted -1.')
 print('(0 0) ->', perceptron.predict(pd.DataFrame({'A': [0], 'B': [0]})), ': excepted -1.')
-plot_values(dict1["A"], dict1["B"], list1)
+plot_values(dict1["A"], dict1["B"], list1, title="Visualization AND door")
 
 dict2 = {'A': [1, 1, 0, 0], 'B': [1, 0, 1, 0]}
 list2 = [1, 1, 1, -1]
@@ -41,13 +41,13 @@ perceptron2 = Perceptron(nb_input=2, learning_rate=0.01, threshold=10)
 
 perceptron2.fit(x_train2, y_train2)
 
-print("*****Testing perceptron for OR dor*****")
+print("*****Testing perceptron for OR door*****")
 print("Input : Output")
 print('(1 1) ->', perceptron2.predict(pd.DataFrame({'A': [1], 'B': [1]})), ': excepted 1.')
 print('(1 0) ->', perceptron2.predict(pd.DataFrame({'A': [1], 'B': [0]})), ': excepted 1.')
 print('(0 1) ->', perceptron2.predict(pd.DataFrame({'A': [0], 'B': [1]})), ': excepted 1.')
 print('(0 0) ->', perceptron2.predict(pd.DataFrame({'A': [0], 'B': [0]})), ': excepted -1.')
-plot_values(dict2["A"], dict2["B"], list2)
+plot_values(dict2["A"], dict2["B"], list2, title="Visualization OR door")
 
 x1 = [0.1, 0.2, 0.5, 0.75, 0.23, 0.45, 13, 10, 9, 12, 11, 10.5]
 x2 = [10, 11, 10.5, 13, 11.5, 12, 0.5, 1, 0.2, 0.3, 1.2, 0.33]
@@ -62,9 +62,9 @@ perceptron3 = Perceptron(nb_input=2, learning_rate=0.01, threshold=10)
 
 perceptron3.fit(x_train3, y_train3)
 
-print("*****Testing perceptron for custom values*****")
+print("*****Testing perceptron for linear separable values*****")
 print('(1 11) ->', perceptron3.predict(pd.DataFrame({'X1': [1], 'X2': [11]})), ': excepted 1.')
 print('(2 12) ->', perceptron3.predict(pd.DataFrame({'X1': [2], 'X2': [12]})), ': excepted 1.')
 print('(10 0) ->', perceptron3.predict(pd.DataFrame({'X1': [10], 'X2': [0]})), ': excepted -1.')
 print('(12 2) ->', perceptron3.predict(pd.DataFrame({'X1': [12], 'X2': [2]})), ': excepted -1.')
-plot_values(x1, x2, list3)
+plot_values(x1, x2, list3, title="Visualization of linear separable values")
